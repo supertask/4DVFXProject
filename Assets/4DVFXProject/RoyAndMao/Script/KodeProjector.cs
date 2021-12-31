fb7804da9009284cf7d2f1f45254333a040247a8
Tasuku Takahashi <lightfox.task@gmail.com>
2022-01-01 03:04:24 +0900

    Added beta's reflection scene

---
+++ Assets/4DVFXProject/RoyAndMao/Script/KodeProjector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Klak.Spout;
+
+namespace Beta
+{
+    public class KodeProjector : MonoBehaviour
+    {
+        #region Editable properties
+
+        [SerializeField] SpoutReceiver _receiver;
+        [SerializeField] Vector2 _extent = Vector2.one;
+        [SerializeField] Vector2 _offset = Vector2.one;
+
+        [SerializeField] float _intensity = 1;
+
+        public float intensity {
+            get { return _intensity; }
+            set { _intensity = value; }
+        }
+
+        #endregion
+
+        #region MonoBehaviour functions
+
+        void Update()
+        {
+            //var vparams = new Vector3(1 / _extent.x, 1 / _extent.y, _intensity);
+            var vparams = new Vector4(1 / _extent.x, 1 / _extent.y, _offset.x, _offset.y);
+            //Shader.SetGlobalVector("KodeLifeParams", vparams);
+            Shader.SetGlobalTexture("KodeLifeMainTex",  _receiver.receivedTexture);
+            Shader.SetGlobalVector("KodeLifeTilingAndOffset", vparams);
+            Shader.SetGlobalFloat("KodeLifeIntensity", _intensity);
+        }
+
+        #endregion
+    }
+}
