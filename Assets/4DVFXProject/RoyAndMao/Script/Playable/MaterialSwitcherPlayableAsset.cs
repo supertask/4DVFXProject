56b48daffbea15b7c3847f973498daed7a99fc63
Tasuku Takahashi <lightfox.task@gmail.com>
2022-01-10 01:16:08 +0900

    DepthKitStudio's script called ShaderGraphLook has a bug that a material is not able to modify by Animation Track. So I use playable asset instead.

---
+++ Assets/4DVFXProject/RoyAndMao/Script/Playable/MaterialSwitcherPlayableAsset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Playables;
+
+using Klak.Motion;
+
+[System.Serializable]
+public class MaterialSwitcherPlayableAsset : PlayableAsset
+{
+    public ExposedReference<GameObject> dancerMeshObj;
+
+
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    {
+        //behaviourに対してパラメータを入れ込んでいく
+        MaterialSwitcherPlayableBehaviour behaviour = new MaterialSwitcherPlayableBehaviour();
+        behaviour.dancerMeshObj = this.dancerMeshObj.Resolve(graph.GetResolver()); //ExposedReferenceからとる時のおまじない
+
+        //behaviour.rotateAngle = rotateAngle;
+
+        return ScriptPlayable<MaterialSwitcherPlayableBehaviour>.Create(graph, behaviour);
+    }
+}
